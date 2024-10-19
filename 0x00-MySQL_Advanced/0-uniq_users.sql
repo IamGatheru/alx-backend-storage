090741595ef10d045057ae8b77346d6aaf472858
IamGatheru <davidgatheru490@gmail.com>
2024-10-19 08:04:35 +0300

    We are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,12 +2,11 @@
 -- id: int, never null, auto increment and primary key
 -- email: string(255-chr), never Null and unique
 -- name: string(255chr)
-CREATE TABLE users (
+CREATE TABLE users IF NOT EXISTS (
 	id int NOT NULL AUTO_INCREMENT,
 	email varchar(255) NOT NULL,
 	name varchar(255),
 	PRIMARY KEY (id),
 	UNIQUE (email)
 );
-CREATE TABLE users IF NOT EXISTS;
 
