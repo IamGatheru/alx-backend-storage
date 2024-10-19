18fe2b1d7b5f48bdc838886915983d07b7ba4d2b
IamGatheru <davidgatheru490@gmail.com>
2024-10-19 08:23:07 +0300

    In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- Users with attributes: id, email, name
+-- id: int, never null, auto increment and primary key
+-- email: string(255-chr), never Null and unique
+-- name: string(255chr)
+CREATE TABLE if NOT EXISTS users (
+	id int NOT NULL AUTO_INCREMENT,
+	email varchar(255) NOT NULL,
+	name varchar(255),
+	country ENUM('US', 'CO','TN') NOT NULL,
+	PRIMARY KEY (id),
+	UNIQUE (email)
+);
+
